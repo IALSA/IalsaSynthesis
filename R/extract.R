95c02e01df8d2d3979d13de9f8ca90cbe8338c9f
Will Beasley <wibeasley@hotmail.com>
2015-10-01 08:04:16 -0700

    describing individual extract functions

--- R/extract.R
+++ R/extract.R
@@ -18,6 +18,7 @@
 #' library(IalsaSynthesis) #Load the package into the current R session.
 
 
+#NOT PARSED BY ROXYGEN: @describeIn extract Generalizable function to return a single string value.
 extract_single_string <- function( regex, source ) {
   matches <- regexpr(regex, source, perl=TRUE);
   result <- attr(matches, "capture.start")[, 1]
@@ -26,23 +27,28 @@ extract_single_string <- function( regex, source ) {
   return( observed_string )
 }
 
+#' @describeIn extract Generalizable function to return a single numeric value.
 extract_single_float <- function( regex, source ) {
   return( as.numeric(extract_single_string(regex, source)) )
 }
 
+#' @describeIn extract Determine the path of the Mplus output file.
 extract_output_filename <- function( mplus_output ) {
   # gsub(pattern="DATA:  File = (.+);", replacement="\\1", mplus_output, perl=T)
   extract_single_string(".+DATA:  File = (.+);.*", mplus_output)
 }
 
+#' @describeIn extract Determine the number of free parameters for the model estimation.
 extract_free_parameter_count <- function( mplus_output ) {
   extract_single_float("Number of Free Parameters\\s+(\\d{1,})\\s+", mplus_output)
 }
 
+#' @describeIn extract Determine the log likelihood for the model estimation.
 extract_loglikelihood <- function( mplus_output ) {
   extract_single_float("Loglikelihood\\s+H0 Value\\s+([-\\d\\.]+)\\s+", mplus_output)
 }
 
+#' @describeIn extract Determine the H0 Scaling Correction Factor for the model estimation.
 extract_scaling_correction <- function( mplus_output ) {
   matched_string <- extract_single_string("\\s+H0 Scaling Correction Factor\\s+([-\\d\\.]+)\\s+for MLR\\s+", mplus_output)
   matched_float <- ifelse(length(matched_string>0), as.numeric(matched_string), NA_real_)
@@ -50,14 +56,17 @@ extract_scaling_correction <- function( mplus_output ) {
   return( matched_float )
 }
 
+#' @describeIn extract Determine the AIC for the model estimation.
 extract_aic <- function( mplus_output ) {
   extract_single_float("Akaike \\(AIC\\)\\s+([-\\d\\.]+)\\s+", mplus_output)
 }
 
+#' @describeIn extract Determine the BIC for the model estimation.
 extract_bic <- function( mplus_output ) {
   extract_single_float("Bayesian \\(BIC\\)\\s+([-\\d\\.]+)\\s+", mplus_output)
 }
 
+#' @describeIn extract Determine the Sample-Size Adjusted BIC for the model estimation.
 extract_bic_adjusted <- function( mplus_output ) {
   # matches <- regexpr("\\s+Sample-Size Adjusted BIC\\s+(\\d+(\\.\\d+)?)\\s+", mplus_output, perl=TRUE);
   # matches <- regexpr("\\s+Sample-Size Adjusted BIC\\s+(.+)\\s+", mplus_output, perl=TRUE);
