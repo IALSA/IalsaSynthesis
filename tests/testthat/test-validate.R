30ac7f43fdb5c7dbd0c2a5e921fdc68b0974504b
Will Beasley <wibeasley@hotmail.com>
2015-12-08 23:29:56 -0600

    dashes instead of underscores in inst/test-data

--- tests/testthat/test-validate.R
+++ tests/testthat/test-validate.R
@@ -4,7 +4,7 @@ context("Validate")
 
 
 test_that("validate_filename_output -good", {   
-  path <- base::file.path(devtools::inst(name="IalsaSynthesis"), "test_data/2015-portland")
+  path <- base::file.path(devtools::inst(name="IalsaSynthesis"), "test-data/2015-portland")
   
   invisible_1 <- validate_filename_output("b1_female_a_grip_categories_4.out", path)
   invisible_2 <- validate_filename_output("b1_female_aehplus_gait_grip.out", path)
@@ -16,7 +16,7 @@ test_that("validate_filename_output -good", {
 })
 
 test_that("validate_filename_output -missing", {   
-  path <- base::file.path(devtools::inst(name="IalsaSynthesis"), "test_data/2015-portland")
+  path <- base::file.path(devtools::inst(name="IalsaSynthesis"), "test-data/2015-portland")
   expected_error_regex <- "The output file was not found at .+"
   expect_error(
     regexp = expected_error_regex,
@@ -25,7 +25,7 @@ test_that("validate_filename_output -missing", {
 })
 
 test_that("validate_filename_output -bad extension", {   
-  path <- base::file.path(devtools::inst(name="IalsaSynthesis"), "test_data/2015-portland")
+  path <- base::file.path(devtools::inst(name="IalsaSynthesis"), "test-data/2015-portland")
   expected_error_regex <- "The output file extension `bad` did not match the expected value of `out`."
   expect_error(
     regexp = expected_error_regex,
