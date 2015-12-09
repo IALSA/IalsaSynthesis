af76f851380074f198c93232f4b684f7027e8bc5
Will Beasley <wibeasley@hotmail.com>
2015-12-08 23:36:42 -0600

    dashes not underscores in utility folder

--- utility/install-packages.R
+++ utility/install-packages.R
@@ -1,15 +1,15 @@
-#This code checks the user's installed packages against the packages listed in `./utility/PackageDependencyList.csv`.
+#This code checks the user's installed packages against the packages listed in `./utility/package-dependency-list.csv`.
 #   These are necessary for the repository's R code to be fully operational. 
 #   CRAN packages are installed only if they're not already; then they're updated if available.
 #   GitHub packages are installed regardless if they're already installed.
-#If anyone encounters a package that should be on there, please add it to `./utility/PackageDependencyList.csv`
+#If anyone encounters a package that should be on there, please add it to `./utility/package-dependency-list.csv`
 
 #Clear memory from previous runs.
 base::rm(list=base::ls(all=TRUE))
 
 #####################################
 ## @knitr DeclareGlobals
-pathCsv <- './utility/PackageDependencyList.csv'
+pathCsv <- './utility/package-dependency-list.csv'
 
 if( !file.exists(pathCsv)) 
   base::stop("The path `", pathCsv, "` was not found.  Make sure the working directory is set to the root of the repository.")
